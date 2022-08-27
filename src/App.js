5a8a6a0a5559db1717c739e0fa0a49e8d520c2e8
ankitsinggh007 <as3824115@gmail.com>
2022-08-27 17:42:46 +0530

    Add rating feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Cards from './components/Cards'
+import Rating from './components/Rating/Rating';
 
 function App() {
   return (
       <div className='Main-Container'>
         <h1>Flip The Card</h1>
         <Cards />
+        <Rating/>
       </div>
   );
 }
